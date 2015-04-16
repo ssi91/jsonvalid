36319eb222a6ee61dceaa1e12f845d2e35d18ec1
ssi <sergeev.s.i1991@gmail.com>
2015-04-17 02:16:08 +0600

    tests

--- maintest.cpp
+++ maintest.cpp
@@ -4,9 +4,20 @@
 
 #include <iostream>
 #include "fsmjson.h"
-int main (){
+
+int main()
+{
+	std::string testStr = "{\"error\":{\"error_code\":5,\"error_msg\":\"User authorization failed: invalid session.\",\"request_params\":[{\"key\":\"oauth\",\"value\":\"1\"},{\"key\":\"method\",\"value\":\"messages.get\"},{\"key\":\"out\",\"value\":\"0\"},{\"key\":\"count\",\"value\":\"1\"},{\"key\":\"user_id\",\"value\":\"9556448\"},{\"key\":\"v\",\"value\":\"5.27\"}]}}";
 	FSMJson fsmJson("{\"@:#\"}[],.\"\"");
-	fsmJson.isValidJson("ddfdf");
-//	std::cout << FSMJson::MARK_DEF;
+	if (fsmJson.isValidJson(testStr.c_str()))
+		std::cout << "valid" << std::endl;
+	size_t state = 0, endIndex;
+	size_t start = 0;
+	state = fsmJson.getNextState(testStr.c_str(), state, endIndex, start);
+	std::cout << state << std::endl;
+	start = endIndex;
+	state = fsmJson.getNextState(testStr.c_str(), state, endIndex, start);
+	std::cout << state << std::endl;
+	std::cout << testStr.substr(start, endIndex - start) << std::endl;
 	return 0;
 }
