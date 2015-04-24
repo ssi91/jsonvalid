0a576886697d17f4f4a8dedc57824e689032baef
ssi <sergeev.s.i1991@gmail.com>
2015-04-24 14:37:16 +0600

    subtotal for search by key

--- maintest.cpp
+++ maintest.cpp
@@ -12,7 +12,10 @@ int main()
 	{
 		std::string testStr = "{\"error\":{\"error_code\":5,\"error_msg\":\"User authorization failed: invalid session.\",\"request_params\":[{\"key\":\"oauth\",\"value\":\"1\"},{\"key\":\"method\",\"value\":\"messages.get\"},{\"key\":\"out\",\"value\":\"0\"},{\"key\":\"count\",\"value\":\"1\"},{\"key\":\"user_id\",\"value\":\"9556448\"},{\"key\":\"v\",\"value\":\"5.27\"}]}}";
 		FSMJson fsmJson("{\"@:#\"}[],.\"\"");
-		fsmJson.setInMap(testStr.c_str());
+		void * qq = fsmJson.setInMap(testStr.c_str(),"error_msg");
+		std::string *ee = static_cast<std::string *>(qq);
+		std::cout << ee[0] << std::endl;
+		delete ee;
 	}
 	catch (RenderException *e)
 	{
